531bc8e4f67a0c3becdba34d23176fd9a5a4caf8
albertoparente <albertoparentefh@gmail.com>
2022-06-09 21:50:14 -0300

    Adding concept

--- delphi-start/Unit1.dfm
+++ delphi-start/Unit1.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 136
-  ClientWidth = 392
+  ClientHeight = 262
+  ClientWidth = 557
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,20 +13,49 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 160
+    Top = 40
+    Width = 8
+    Height = 13
+    Caption = '='
+  end
   object Button1: TButton
-    Left = 296
-    Top = 64
+    Left = 474
+    Top = 229
     Width = 75
     Height = 25
     Caption = 'Button1'
     TabOrder = 0
     OnClick = Button1Click
   end
-  object edName: TEdit
-    Left = 16
-    Top = 16
-    Width = 257
+  object edValue1: TEdit
+    Left = 14
+    Top = 8
+    Width = 115
     Height = 21
     TabOrder = 1
+  end
+  object edValue2: TEdit
+    Left = 14
+    Top = 72
+    Width = 115
+    Height = 21
+    TabOrder = 2
+  end
+  object edResult: TEdit
+    Left = 216
+    Top = 37
+    Width = 113
+    Height = 21
+    TabOrder = 3
+  end
+  object Button2: TButton
+    Left = 14
+    Top = 120
+    Width = 49
+    Height = 25
+    Caption = '+'
+    TabOrder = 4
   end
 end
--- delphi-start/Unit1.pas
+++ delphi-start/Unit1.pas
@@ -3,25 +3,32 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
-    edName: TEdit;
+    edValue1: TEdit;
+    edValue2: TEdit;
+    Label1: TLabel;
+    edResult: TEdit;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
-      i : integer;
-      c : currency;
-      f : double;
-      r : real;
-      s : string;
-      b : boolean;
+    i: integer;
+    c: currency;
+    f: double;
+    r: real;
+    s: string;
+    b: boolean;
+    Date : TDateTime;
 
-      procedure ShowVariable(Value : string);
-      function  showText : string;
+    procedure ShowVariable(Value: Integer);
+    function showText: string;
+    function sum(value1, value2 : Integer) : Integer;
   public
     { Public declarations }
 
@@ -29,6 +36,7 @@ type
 
 var
   Form1: TForm1;
+  Global : String;
 
 implementation
 
@@ -40,14 +48,22 @@ begin
   c := 1.1;
   f := 5.43;
   r := 6.543;
-  //s := edName.Text;
+  // s := edName.Text;
   b := true;
-  ShowVariable(s);
+
+
+  Global := showText;
+  ShowVariable(StrToInt(showText));
 end;
 
-procedure TForm1.ShowVariable(Value: string);
+procedure TForm1.ShowVariable(Value: Integer);
 begin
-  ShowMessage(Value);
+  ShowMessage(IntToStr(value));
+end;
+
+function TForm1.sum(value1, value2: Integer): Integer;
+begin
+
 end;
 
 function TForm1.showText;
