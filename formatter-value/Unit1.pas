e10badad9a2d9bf7a91e78fcd55908fddb876367
albertoparente <albertoparentefh@gmail.com>
2022-06-27 22:51:38 -0300

    Adding formatter value

---
+++ formatter-value/Unit1.pas
@@ -0,0 +1,35 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Value : Currency;
+begin
+  Value := StrToCurr(Edit1.Text);
+  Edit1.Text := FormatCurr('##,##.00', Value);
+end;
+
+end.
