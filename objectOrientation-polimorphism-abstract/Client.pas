8670df998dafd8166f64a3852e8d8fdc020fcf7f
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-06 20:42:28 -0300

    Adding implementation

--- objectOrientation-polimorphism-abstract/Client.pas
+++ objectOrientation-polimorphism-abstract/Client.pas
@@ -12,5 +12,13 @@ type
     property CPF: string read FCPF write FCPF;
     function ReturnDados: String; Override;
   end;
+  
+function TClient.ReturnDados: String;
+begin
+  Result := 'Name: ' + Self.Name +
+            ', Brith Day: ' + Self.BrithDay +
+            ', Age: ' + Self.Age.ToString +
+            ', CPF: ' + FCpf;
+end;
 
 end.
