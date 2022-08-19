6028b8d395b17f63c424fb40bc2ede401fcd728b
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-18 23:21:54 -0300

    Adding Implementation

--- objectOrientation-polimorphism-virtual-override/VirtualClient.pas
+++ objectOrientation-polimorphism-virtual-override/VirtualClient.pas
@@ -10,8 +10,12 @@ type
   TClient = class(TPerson)
   private
     FCPF: String;
+    FEmail: String;
+    FSexo: String;
   public
     property CPF: string read FCPF write FCPF;
+    property Email: string read FEmail write FEmail;
+    property Sexo: string read FSexo write FSexo;
     function ReturnDados: String; Override;
   end;
 
@@ -24,7 +28,9 @@ begin
   Result := 'Name: ' + Self.Name +
             ', Brith Day: ' + Self.BrithDay +
             ', Age: ' + Self.Age.ToString +
-            ', CPF: ' + FCpf;
+            ', CPF: ' + FCpf +
+            ', Email: ' + FEmail +
+            ', Sexo: ' + FSexo;
 end;
 
 end.
