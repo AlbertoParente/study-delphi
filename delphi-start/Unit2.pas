8c0c0782869274682e7bfb98f38876378c432d40
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-22 23:50:34 -0300

    Adding implementation

--- delphi-start/Unit2.pas
+++ delphi-start/Unit2.pas
@@ -45,4 +45,54 @@ implementation
 
 {$R *.dfm}
 
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if CheckBox1.Checked then
+    ShowMessage('True')
+  else if CheckBox2.Checked then
+    ShowMessage('CheckBox2 is True!')
+  else
+    ShowMessage('False');
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  Returnn: Integer;
+begin
+  Returnn := MessageDlg('Do you want to continue the apllication?',
+    TMsgDlgType.mtConfirmation, [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], 0);
+  if Returnn = 6 then
+    ShowMessage('Welcome!')
+  else
+    Close;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  if 100 > 800 then
+    ShowMessage('1000 is >');
+
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  if (1 = 1) or (2 = 3) then
+    ShowMessage('error');
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  if 1 = 1 then
+  begin
+    ShowMessage('True');
+    ShowMessage('True, True');
+  end
+  else
+  begin
+    ShowMessage('Error');
+    ShowMessage('Error, Error');
+  end;
+end;
+
 end.
