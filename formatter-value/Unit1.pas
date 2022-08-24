9efc172ce1186c3088226d32d83ef23619eded69
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-23 22:35:22 -0300

    Adding implementation

--- formatter-value/Unit1.pas
+++ formatter-value/Unit1.pas
@@ -27,9 +27,15 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   Value : Currency;
+  CNPJ : String;
+  CPF : String;
+  IE : String;
 begin
   Value := StrToCurr(Edit1.Text);
   Edit1.Text := FormatCurr('##,##.00', Value);
+  CNPJ := FormatCurr('##.###.###/####.##', Value);
+  CPF := FormatCurr('###.###.###-##', Value);
+  IE := FormatCurr('###.###.###.##', Value);
 end;
 
 end.
