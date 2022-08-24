0e08fa79184092656c960efa7af188edf5489656
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-24 19:34:20 -0300

    Adding contracts

--- objectOrientation-polimorphism-abstract/User.pas
+++ objectOrientation-polimorphism-abstract/User.pas
@@ -9,6 +9,9 @@ type
   private
   public
     function contract: String; override;
+    function contract2: String; override;
+    function contract3: String; override;
+    function contract4: String; override;
   end;
 
 implementation
@@ -18,6 +21,21 @@ implementation
 function TUser.contract: String;
 begin
   Result := 'TUser Contract';
+end;
+
+function TUser.contract2: String;
+begin
+  Result := 'TUser Contract 2';
+end;
+
+function TUser.contract3: String;
+begin
+  Result := 'TUser Contract 3';
+end;
+
+function TUser.contract4: String;
+begin
+  Result := 'TUser Contract 4';
 end;
 
 end.
