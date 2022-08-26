ccd9dd3d33d7500359e300f16e35d73d7a62ef3c
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-08-25 21:54:54 -0300

    Adding new person

--- objectOrientation-encapsulation/Main.pas
+++ objectOrientation-encapsulation/Main.pas
@@ -30,14 +30,35 @@ uses
 procedure TForm3.Button1Click(Sender: TObject);
 var
   person : TPerson;
+  person1 : TPerson;
+  person2 : TPerson;
 begin
   person := TPerson.Create;
+  person1 := TPerson.Create;
+  person2 := TPerson.Create;
+  
   try
+    ShowMessage('=====================================================');
     person.Name      := 'Alberto Parente';
     person.BirthDate := '13/08/1998';
     ShowMessage(person.Name + ' - ' + person.Age.CalcAge(person.BirthDate).ToString);
+    ShowMessage('=====================================================');
+    
+    ShowMessage('=====================================================');
+    person1.Name      := 'Juliana Cavalcante';
+    person1.BirthDate := '13/08/1998';
+    ShowMessage(person1.Name + ' - ' + person1.Age.CalcAge(person1.BirthDate).ToString);
+    ShowMessage('=====================================================');
+    
+    ShowMessage('=====================================================');
+    person2.Name      := 'Julia Parente';
+    person2.BirthDate := '13/08/1998';
+    ShowMessage(person2.Name + ' - ' + person2.Age.CalcAge(person2.BirthDate).ToString);
+    ShowMessage('=====================================================');
   finally
     person.Free;
+    person1.Free;
+    person2.Free;
   end;
 end;
 
